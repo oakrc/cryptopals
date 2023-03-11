559ebd447d3511a73b6f8c98beddcd3240cfcd86
Zhenkai Weng <theoaktree1040@gmail.com>
2023-03-10 19:07:40 -0800

    30

--- 30.py
+++ 30.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+#!/usr/bin/env python
+from md4 import MD4
 import secrets
 import struct
-from mycrypto import sha1_keyed_hmac
-from sha1 import SHA1
+
 
 
 # ===========
@@ -11,14 +12,21 @@ from sha1 import SHA1
 key = secrets.token_bytes(20)
 
 
+def md4_keyed_hmac(message: bytes, key: bytes) -> bytes:
+    md4 = MD4()
+    md4.update(key)
+    md4.update(message)
+    return md4.digest()
+
+
 def generate() -> tuple[bytes, bytes]:
     """Generate a valid message-HMAC pair using a secret key."""
     message = b'comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon'
-    return (message, sha1_keyed_hmac(message, key))
+    return (message, md4_keyed_hmac(message, key))
 
 
 def validate_hmac(message: bytes, hmac: bytes) -> bool:
-    return sha1_keyed_hmac(message, key) == hmac
+    return md4_keyed_hmac(message, key) == hmac
 
 
 # ========
@@ -33,19 +41,16 @@ def attack(message: bytes, hmac: bytes):
         new_message = message + pad + suffix
         processed_message_len = key_len + len(message) + len(pad)
 
-        sha1 = SHA1.continue_from(hmac, processed_message_len)
-        sha1.update(suffix)
-        new_hmac = sha1.digest()
+        md4 = MD4.continue_from(hmac, processed_message_len)
+        md4.update(suffix)
+        new_hmac = md4.digest()
 
         if validate_hmac(new_message, new_hmac):
             print(f'Succeeded with key length {key_len}')
 
 
 def glue_pad(l: int) -> bytes:
-    pad = b'\x80'
-    pad += b'\x00' * ((56 - (l + 1) % 64) % 64)
-    pad += struct.pack(b'>Q', l * 8)
-    return pad
+    return  b'\x80' + b'\x00' * ((55 - l) % 64) + struct.pack('<Q', l * 8)
 
 
 if __name__ == '__main__':
